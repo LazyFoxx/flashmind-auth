fb16e501b5e91030a88436370a09ce69a5400f05
LazyFoxx <chernomor987654321@gmail.com>
2026-01-02 12:25:56 +0300

    обновил настройки

--- src/config/settings.py
+++ src/config/settings.py
@@ -3,16 +3,27 @@ from pydantic_settings import BaseSettings, SettingsConfigDict
 
 
 class DatabaseConfig(BaseModel):
-    url: PostgresDsn
     echo: bool = False
     echo_pool: bool = False
     pool_size: int = 10
     max_overflow: int = 10
 
+    user: str
+    password: str
+    db: str
+    host: str
+    port: int
+
+    @property
+    def url(self) -> PostgresDsn:
+        """Собранный URL с валидацией"""
+        url_str = f"postgresql+asyncpg://{self.user}:{self.password}@{self.host}:{self.port}/{self.db}"
+        return PostgresDsn(url_str)
+
 
 class Settings(BaseSettings):
     model_config = SettingsConfigDict(
-        case_sensitive=False, env_nested_delimiter="__", env_prefix="APP_CONFIG__"
+        case_sensitive=False,
     )
 
     db: DatabaseConfig
