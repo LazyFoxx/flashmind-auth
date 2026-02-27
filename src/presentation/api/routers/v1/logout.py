935876ac8e5969cc8d6813edea690f62fdd43393
LazyFoxx <chernomor987654321@gmail.com>
2026-02-27 11:30:02 +0300

    фикс logout

--- src/presentation/api/routers/v1/logout.py
+++ src/presentation/api/routers/v1/logout.py
@@ -1,6 +1,5 @@
 from fastapi import APIRouter, Depends, status, Response
 from dishka.integrations.fastapi import FromDishka, inject
-from src.presentation.api.dto.auth import MessageResponse
 from src.secure.dependencies import get_current_user
 from src.domain.entities.user import User
 
@@ -11,8 +10,7 @@ router = APIRouter(tags=["logout"])
 
 @router.post(
     "/logout",
-    status_code=status.HTTP_200_OK,
-    response_model=MessageResponse,
+    status_code=status.HTTP_204_NO_CONTENT,
     summary="Выход с авторизации",
     description=(
         "Делает все токены не действительными, удаляет рефреш токен из cookie и завершает сессию."
@@ -23,7 +21,7 @@ async def logout(
     response: Response,
     use_case: FromDishka[LogoutUseCase],
     user: User = Depends(get_current_user),
-) -> MessageResponse:
+) -> None:
     await use_case.execute(user_id=user.id)
 
     response.delete_cookie(
@@ -32,4 +30,4 @@ async def logout(
         path="/",
     )
 
-    return MessageResponse(message="Вы успешно вышли с авторизации")
+    return None
